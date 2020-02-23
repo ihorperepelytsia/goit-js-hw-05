2c1030d67ef911d90b8db99465524450e04d8c8c
Ihor Perepelytsia <igorekperepelitsa@gmail.com>
2020-02-23 20:35:11 +0200

    completed module5

---
+++ task-4.js
@@ -0,0 +1,38 @@
+"use strict";
+
+class StringBuilder {
+  constructor(str) {
+    this._value = str;
+  }
+  get value() {
+    return this._value;
+  }
+
+  set value(value) {
+    this._value = value;
+  }
+
+  append(str) {
+    this._value = this._value + str;
+  }
+
+  prepend(str) {
+      this._value = str + this._value
+  }
+
+  pad(str) {
+    this._value = this._value + str;
+    this._value = str + this._value
+  }
+}
+
+const builder = new StringBuilder(".");
+
+builder.append("^");
+console.log(builder.value); // '.^'
+
+builder.prepend("^");
+console.log(builder.value); // '^.^'
+
+builder.pad("=");
+console.log(builder.value); // '=^.^='
